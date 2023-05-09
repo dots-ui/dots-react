3e45a78bfd41624c501429239f3915e3ef7c00b1
Mohammad <mo@mohammad.dev>
2023-05-08 23:09:19 -0600

    style(text): prettify

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -15,41 +15,40 @@ export const Text = (props: TextProps) => {
   return <TextElement as={variant} {...props} />;
 };
 
-
 const variantMapping = {
   h1: {
     fontSize: "2em",
-    fontWeight: 700
+    fontWeight: 700,
   },
   h2: {
     fontSize: "1.5em",
-    fontWeight: 600
+    fontWeight: 600,
   },
   h3: {
     fontSize: "1.17em",
-    fontWeight: 600
+    fontWeight: 600,
   },
   h4: {
     fontSize: "1em",
-    fontWeight: 500
+    fontWeight: 500,
   },
   h5: {
     fontSize: "0.83em",
-    fontWeight: 500
+    fontWeight: 500,
   },
   h6: {
     fontSize: "0.67em",
-    fontWeight: 500
+    fontWeight: 500,
   },
   p: {
     fontSize: "1em",
-    fontWeight: 400
-  }
+    fontWeight: 400,
+  },
 };
 
 const TextElement = styled.p<TextProps>`
-    font-family: "Poppins", sans-serif;
-    font-weight: ${(props) => variantMapping[props.variant ?? "p"].fontWeight};
-    font-size: ${(props) => variantMapping[props.variant ?? "p"].fontSize};
-    color: ${(props) => props.theme.text};
-  `;
+  font-family: "Poppins", sans-serif;
+  font-weight: ${(props) => variantMapping[props.variant ?? "p"].fontWeight};
+  font-size: ${(props) => variantMapping[props.variant ?? "p"].fontSize};
+  color: ${(props) => props.theme.text};
+`;
