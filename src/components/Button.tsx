5f9fdb79cc7cc68fe0cf648f8e94bde1d90a3335
Mohammad <mo@mohammad.dev>
2023-05-08 21:05:14 -0600

    fix(button): check if creating button in separate function results in next.js warning being thrown
    In next.js, having styled components that get created during a component lifecycle will cause a
    warning to pop up. I'm fairly certain this won't fix the issue at hand but it would be an easy fix
    if it did.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -17,96 +17,98 @@ export type ButtonProps = React.HTMLAttributes<
 
 export const Button = (props: ButtonProps) => {
   const { variant, accent } = props;
-  const ButtonElement = styled(props.href ? "a" : "button")<ButtonProps>`
-    font-size: ${(props) => {
-      if (props.size === "small") return "0.75em";
-      if (props.size === "large") return "1.25em";
-      return "1em";
-    }};
-    font-weight: 400;
-    padding: ${(props) => {
-      if (props.size === "small") return "0.5em 1em";
-      if (props.size === "large") return "1em 2em";
-      return "0.75em 1.5em";
-    }};
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
-    transition: all 0.2s ease-in-out;
-    text-decoration: none;
-    width: ${(props) => {
-      if (props.shape === "rounded") {
-        if (props.size === "small") return "2.5em";
-        if (props.size === "large") return "6em";
-        return "4em";
-      }
-      return "auto";
-    }};
-    height: ${(props) => {
-      if (props.shape === "rounded") {
-        if (props.size === "small") return "2.5em";
-        if (props.size === "large") return "6em";
-        return "4em";
-      }
-      return "auto";
-    }};
-    border-radius: ${(props) => {
-      if (props.shape === "square" || !props.shape) return "0.375em";
-      if (props.size === "small") return "1.25em";
-      if (props.size === "large") return "3em";
-      return "2em";
-    }};
+  const ButtonElement = createButtonElement(props);
+  return <ButtonElement {...props} />;
+};
+
+const createButtonElement = (props: ButtonProps) => styled(
+  props.href ? "a" : "button"
+)<ButtonProps>`
+  font-size: ${(props) => {
+    if (props.size === "small") return "0.75em";
+    if (props.size === "large") return "1.25em";
+    return "1em";
+  }};
+  font-weight: 400;
+  padding: ${(props) => {
+    if (props.size === "small") return "0.5em 1em";
+    if (props.size === "large") return "1em 2em";
+    return "0.75em 1.5em";
+  }};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  width: ${(props) => {
+    if (props.shape === "rounded") {
+      if (props.size === "small") return "2.5em";
+      if (props.size === "large") return "6em";
+      return "4em";
+    }
+    return "auto";
+  }};
+  height: ${(props) => {
+    if (props.shape === "rounded") {
+      if (props.size === "small") return "2.5em";
+      if (props.size === "large") return "6em";
+      return "4em";
+    }
+    return "auto";
+  }};
+  border-radius: ${(props) => {
+    if (props.shape === "square" || !props.shape) return "0.375em";
+    if (props.size === "small") return "1.25em";
+    if (props.size === "large") return "3em";
+    return "2em";
+  }};
+  border: ${(props) => {
+    if (props.variant !== "outlined") return "none";
+    return `1px solid ${
+      props.theme[props.accent || "info"] ?? getThemeValue(props.accent || "info")
+    }`;
+  }};
+  background-color: ${(props) => {
+    if (props.variant === "contained")
+      return props.theme[props.accent || "info"] ?? getThemeValue(props.accent || "info");
+    return "transparent";
+  }};
+  color: ${(props) => {
+    if (props.variant === "contained") return "#fff";
+    return props.theme[props.accent || "info"] ?? getThemeValue(props.accent || "info");
+  }};
+
+  &:disabled {
     border: ${(props) => {
-      if (variant !== "outlined") return "none";
-      return `1px solid ${
-        props.theme[accent || "info"] ?? getThemeValue(accent || "info")
-      }`;
+      if (props.variant !== "outlined") return "none";
+      return `1px solid ${props.theme.disabled ?? getThemeValue("disabled")}`;
     }};
     background-color: ${(props) => {
-      if (variant === "contained")
-        return props.theme[accent || "info"] ?? getThemeValue(accent || "info");
+      if (props.variant === "contained")
+        return props.theme.disabled ?? getThemeValue("disabled");
       return "transparent";
     }};
     color: ${(props) => {
-      if (variant === "contained") return "#fff";
-      return props.theme[accent || "info"] ?? getThemeValue(accent || "info");
+      if (props.variant === "contained") return "#fff";
+      return props.theme.disabled ?? getThemeValue("disabled");
     }};
+  }
 
-    &:disabled {
-      border: ${(props) => {
-        if (variant !== "outlined") return "none";
-        return `1px solid ${props.theme.disabled ?? getThemeValue("disabled")}`;
-      }};
-      background-color: ${(props) => {
-        if (variant === "contained")
-          return props.theme.disabled ?? getThemeValue("disabled");
-        return "transparent";
-      }};
-      color: ${(props) => {
-        if (variant === "contained") return "#fff";
-        return props.theme.disabled ?? getThemeValue("disabled");
-      }};
-    }
-
-    &:hover:not(:disabled) {
-      filter: drop-shadow(
-          0em 0.125em 0.625em
-            ${(props) =>
-              props.theme[accent || "info"] ??
-              getThemeValue(accent || "info")}80
-        )
-        brightness(110%);
-    }
-    &:active:not(:disabled) {
-      filter: drop-shadow(
-          0em 0.0625em 0.3125em
-            ${(props) =>
-              props.theme[accent || "info"] ??
-              getThemeValue(accent || "info")}80
-        )
-        brightness(80%);
-    }
-  `;
-  return <ButtonElement {...props} />;
-};
+  &:hover:not(:disabled) {
+    filter: drop-shadow(
+        0em 0.125em 0.625em
+          ${(props) =>
+            props.theme[props.accent || "info"] ?? getThemeValue(props.accent || "info")}80
+      )
+      brightness(110%);
+  }
+  &:active:not(:disabled) {
+    filter: drop-shadow(
+        0em 0.0625em 0.3125em
+          ${(props) =>
+            props.theme[props.accent || "info"] ?? getThemeValue(props.accent || "info")}80
+      )
+      brightness(80%);
+  }
+`;
