975bd6723ce509e89a1d129e841ad29a029f33dc
Mohammad <mo@mohammad.dev>
2023-05-11 23:34:22 -0600

    fix(next): try injecting styles via a component registry

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export { Background } from './components/Background';
 export { TextField, type TextFieldProps } from './components/TextField';
 export { Text, type TextProps } from './components/Text';
 export { ThemeProvider } from './components/ThemeProvider';
+export { DotsUIComponentRegistry } from './components/next';
